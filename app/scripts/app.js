b4f05538edaac8c0597ca2f36091d11894694a87
Sam Eureka <sameureka@me.com>
2015-09-29 16:41:25 -0400

    fixed time bug, added display function

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,57 +1,47 @@
 // Global variables
 var interval = null;
-var time = 25;
+var time = 65;
 var onBreak = false;
 
 // Starts the work timer interval and calls workTimer function
 var startWork = function(){
   interval = setInterval(workTimer, 1000);
-  workTimer();
   $('#btn').attr({'class': 'btn btn-md btn-danger btn-block', value: 'Reset', onclick:'resetTimer()'});
 };
 
 // Starts the break timer interval and calls breakTimer
 var startBreak = function(){  
   interval = setInterval(breakTimer, 1000);
-  breakTimer();
   $('#btn').attr({'class': 'btn btn-md btn-danger btn-block', value: 'Reset', onclick:'resetTimer()'});
 };
 
 // Decrements work time. Display the remaining time
 var workTimer = function(){
-    if (time === 0){
-      clearInterval(interval);
-      setBreak();
-    }  
-    time--;
-    $('#displayNow').html(formatTime(time));
+  time === 0 ? setBreak() : time--;
+  displayTime(time);
 };
 
 // Decrements break time. Display it... 
 var breakTimer = function(){
-  if (time === 0){
-    clearInterval(interval);
-    resetTimer();
-  }
-  time--;
-  $('#displayNow').html(formatTime(time));
+  time === 0 ? resetTimer() : time--;
+  displayTime(time);
 };
 
 // Resets the interval and display
 var resetTimer = function(){
   clearInterval(interval);
-  time = 26;
+  time = 65;
   onBreak = false;
-  $('#displayNow').html(formatTime(time));
+  displayTime(time);
   $('#btn').attr({'class': 'btn btn-md btn-primary btn-block', value: 'Work', onclick: 'startWork()'});
 };
 
 // Set up the break
 var setBreak = function(){
   clearInterval(interval);
-  time = 6;
+  time = 10;
   onBreak = true;
-  $('#displayNow').html(formatTime(time));
+  displayTime(time);
   $('#btn').attr({'class': 'btn btn-md btn-success btn-block', value: 'Break', onclick: 'startBreak()'});
 };
 
@@ -59,12 +49,18 @@ var setBreak = function(){
 function formatTime(seconds){
     var min = Math.floor(seconds/60);
     var sec = Math.floor(seconds%60);
-  // if (min < 10){min = '0'+min;}
-  if (sec < 10){sec = '0'+sec;}
+  min === 0 ? min = "" : min;
+  sec < 10 ? sec = '0'+sec : sec;
   return min+':'+sec;
+}
+
+// Sends time to the DOM
+function displayTime(time){
+ $('#displayNow').html(formatTime(time));
 }
 
 // Sets everything up when the document is ready
 $(document).ready(function(){
-  $('#displayNow').html(formatTime(time));
+  displayTime(time);
+  //$('#displayNow').html(formatTime(time));
 });
