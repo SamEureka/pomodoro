53cdb31ba725f6ec787e484135d6809fd95801bc
Sam Eureka <sameureka@me.com>
2015-09-29 12:32:43 -0400

    user story 2 working, still vanilla JS ¯\_(ツ)_/¯

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,50 +1,66 @@
 // Global variables
-var setInc = null;
-var time = 1500;
-var breakSeconds = 300;
+var interval = null;
+var time = 20;
 var onBreak = false;
 
-// Starts the timer interval and calls countdown function
-var startTimer = function(){
-  if(onBreak === true){ 
-    setInc = setInterval(breakTime, 1000);
-    breakTime();
-  } else {
-    setInc = setInterval(countdown, 1000);
-    countdown();
-}
-  $('#btn').attr('value', 'Reset');
-  $('#btn').attr('onclick','resetTimer()');
+// Starts the work timer interval and calls workTimer function
+var startWork = function(){
+  interval = setInterval(workTimer, 1000);
+  workTimer();
+  $('#btn').attr({value: 'Reset', onclick:'resetTimer()'});
 };
 
-// Decrements time. Calls formatTime function and sends output to display  
-var countdown = function(){
+// Starts the break timer interval and calls breakTimer
+var startBreak = function(){  
+  interval = setInterval(breakTimer, 1000);
+  breakTimer();
+  $('#btn').attr({value: 'Reset', onclick:'resetTimer()'});
+};
+
+var workTimer = function(){
+    if (time === 0){
+      clearInterval(interval);
+      setBreak();
+    }  
+    time--;
+    $('#displayNow').html(formatTime(time));
+};
+
+// Decrements break time. Displays it... I think there is a better way to do this.
+var breakTimer = function(){
+  if (time === 0){
+    clearInterval(interval);
+    resetTimer();
+  }
   time--;
   $('#displayNow').html(formatTime(time));
 };
 
-var countdown = function(){
-  breakSeconds--;
-  $('#displayNow').html(formatTime(breakSeconds));
-};
-
 // Resets the interval and display
-function resetTimer(){
-  clearInterval(setInc);
-  time = 1500;
+var resetTimer = function(){
+  clearInterval(interval);
+  time = 21;
   onBreak = false;
   $('#displayNow').html(formatTime(time));
-  $('#btn').attr('value', 'Start');
-  $('#btn').attr('onclick','startTimer()');
-}
+  $('#btn').attr({value: 'Work', onclick: 'startWork()'});
+};
+
+// Set up the break
+var setBreak = function(){
+  clearInterval(interval);
+  time = 6;
+  onBreak = true;
+  $('#displayNow').html(formatTime(time));
+  $('#btn').attr({value: 'Break', onclick: 'startBreak()'});
+};
 
 // Converts total seconds into minutes and seconds in the proper display format -XX:XX
 function formatTime(seconds){
     var min = Math.floor(seconds/60);
     var sec = Math.floor(seconds%60);
-  if (min < 10){min = '0'+min;}
+  // if (min < 10){min = '0'+min;}
   if (sec < 10){sec = '0'+sec;}
-  return '-'+min+':'+sec;
+  return min+':'+sec;
 }
 
 // Sets everything up when the document is ready
