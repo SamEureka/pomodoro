a8e290635590d758bf64c9b4f8860d3d05ff7908
Sam Eureka <sameureka@me.com>
2015-09-29 10:12:39 -0400

    adding in some state, broken

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,34 +1,47 @@
 // Global variables
 var setInc = null;
 var time = 1500;
+var breakSeconds = 300;
+var onBreak = false;
 
 // Starts the timer interval and calls countdown function
 var startTimer = function(){
-  setInc = setInterval(countdown, 1000);
-  countdown();
+  if(onBreak === true){ 
+    setInc = setInterval(breakTime, 1000);
+    breakTime();
+  } else {
+    setInc = setInterval(countdown, 1000);
+    countdown();
+}
   $('#btn').attr('value', 'Reset');
   $('#btn').attr('onclick','resetTimer()');
 };
 
 // Decrements time. Calls formatTime function and sends output to display  
-var countdown = function(){  
+var countdown = function(){
   time--;
   $('#displayNow').html(formatTime(time));
+};
+
+var countdown = function(){
+  breakSeconds--;
+  $('#displayNow').html(formatTime(breakSeconds));
 };
 
 // Resets the interval and display
 function resetTimer(){
   clearInterval(setInc);
   time = 1500;
+  onBreak = false;
   $('#displayNow').html(formatTime(time));
   $('#btn').attr('value', 'Start');
   $('#btn').attr('onclick','startTimer()');
 }
 
 // Converts total seconds into minutes and seconds in the proper display format -XX:XX
-function formatTime(time){
-    var min = Math.floor(time/60);
-    var sec = Math.floor(time%60);
+function formatTime(seconds){
+    var min = Math.floor(seconds/60);
+    var sec = Math.floor(seconds%60);
   if (min < 10){min = '0'+min;}
   if (sec < 10){sec = '0'+sec;}
   return '-'+min+':'+sec;
